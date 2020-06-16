1802c443e2400860cba2218dfbd682e9bc0845e9
Sam Evans <evansst@icloud.com>
2020-06-16 04:38:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word (string)
   array = string.split(/ /)
   array.collect do |element|
     element = element.reverse 
-    binding.pry
+    new_array << element
   end
 
   output = array.join(" ")
