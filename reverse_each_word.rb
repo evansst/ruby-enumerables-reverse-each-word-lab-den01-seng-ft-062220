770671d417ff99eec29a9f5a42077efb22847005
Sam Evans <evansst@icloud.com>
2020-06-16 04:37:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word (string)
   array = string.split(/ /)
   array.collect do |element|
-    element = element.reverse 
+    element.reverse 
   end
   binding.pry
   output = array.join(" ")
